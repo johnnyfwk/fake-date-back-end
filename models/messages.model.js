6d939f835a8272cc43cf70a390f8f416b5678e9e
johnnyfwk <johnnyfong@live.com>
2023-03-16 15:41:44 +0000

    messages.model.js: delete messages by sender only, not receiver.

--- models/messages.model.js
+++ models/messages.model.js
@@ -154,10 +154,7 @@ function deleteAllMessagesByUserId(userId) {
 
     const queryString = `
         DELETE FROM messages
-        WHERE
-            sender_user_id = $1
-        OR
-            receiver_user_id = $1
+        WHERE sender_user_id = $1
         RETURNING *;
     `
     const queryValue = [userId];
