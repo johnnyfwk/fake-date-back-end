867526e5cbdc0571350ac6e75dcea12b30565f68
johnnyfwk <johnnyfong@live.com>
2023-03-20 16:49:33 +0000

    Changed mock users' passwords.

--- database/data/development/users.js
+++ database/data/development/users.js
@@ -1,14 +1,14 @@
 const users = [
-    {username: "johnny", password: "password", gender: "Male", avatarUrl: "http://cdn.onlinewebfonts.com/svg/img_258083.png", joinDate: "2023-03-10T00:59:42.783Z"},
-    {username: "abbie", password: "password", gender: "Female", avatarUrl: "http://cdn.onlinewebfonts.com/svg/img_258083.png", joinDate: "2023-03-10T01:59:42.783Z"},
-    {username: "billy", password: "password", gender: "Male", avatarUrl: "http://cdn.onlinewebfonts.com/svg/img_258083.png", joinDate: "2023-03-10T02:59:42.783Z"},
-    {username: "catherine", password: "password", gender: "Female", avatarUrl: "http://cdn.onlinewebfonts.com/svg/img_258083.png", joinDate: "2023-03-10T03:59:42.783Z"},
-    {username: "david", password: "password", gender: "Male", avatarUrl: "http://cdn.onlinewebfonts.com/svg/img_258083.png", joinDate: "2023-03-10T04:59:42.783Z"},
-    {username: "ellie", password: "password", gender: "Female", avatarUrl: "http://cdn.onlinewebfonts.com/svg/img_258083.png", joinDate: "2023-03-10T05:59:42.783Z"},
-    {username: "frank", password: "password", gender: "Male", avatarUrl: "http://cdn.onlinewebfonts.com/svg/img_258083.png", joinDate: "2023-03-10T06:59:42.783Z"},
-    {username: "gary", password: "password", gender: "Male", avatarUrl: "http://cdn.onlinewebfonts.com/svg/img_258083.png", joinDate: "2023-03-10T07:59:42.783Z"},
-    {username: "helen", password: "password", gender: "Female", avatarUrl: "http://cdn.onlinewebfonts.com/svg/img_258083.png", joinDate: "2023-03-10T08:59:42.783Z"},
-    {username: "ian", password: "password", gender: "Male", avatarUrl: "http://cdn.onlinewebfonts.com/svg/img_258083.png", joinDate: "2023-03-10T09:59:42.783Z"},
+    {username: "johnny", password: "internick", gender: "Male", avatarUrl: "http://cdn.onlinewebfonts.com/svg/img_258083.png", joinDate: "2023-03-10T00:59:42.783Z"},
+    {username: "abbie", password: "internick", gender: "Female", avatarUrl: "http://cdn.onlinewebfonts.com/svg/img_258083.png", joinDate: "2023-03-10T01:59:42.783Z"},
+    {username: "billy", password: "internick", gender: "Male", avatarUrl: "http://cdn.onlinewebfonts.com/svg/img_258083.png", joinDate: "2023-03-10T02:59:42.783Z"},
+    {username: "catherine", password: "internick", gender: "Female", avatarUrl: "http://cdn.onlinewebfonts.com/svg/img_258083.png", joinDate: "2023-03-10T03:59:42.783Z"},
+    {username: "david", password: "internick", gender: "Male", avatarUrl: "http://cdn.onlinewebfonts.com/svg/img_258083.png", joinDate: "2023-03-10T04:59:42.783Z"},
+    {username: "ellie", password: "internick", gender: "Female", avatarUrl: "http://cdn.onlinewebfonts.com/svg/img_258083.png", joinDate: "2023-03-10T05:59:42.783Z"},
+    {username: "frank", password: "internick", gender: "Male", avatarUrl: "http://cdn.onlinewebfonts.com/svg/img_258083.png", joinDate: "2023-03-10T06:59:42.783Z"},
+    {username: "gary", password: "internick", gender: "Male", avatarUrl: "http://cdn.onlinewebfonts.com/svg/img_258083.png", joinDate: "2023-03-10T07:59:42.783Z"},
+    {username: "helen", password: "internick", gender: "Female", avatarUrl: "http://cdn.onlinewebfonts.com/svg/img_258083.png", joinDate: "2023-03-10T08:59:42.783Z"},
+    {username: "ian", password: "internick", gender: "Male", avatarUrl: "http://cdn.onlinewebfonts.com/svg/img_258083.png", joinDate: "2023-03-10T09:59:42.783Z"},
 ];
 
 module.exports = users;
